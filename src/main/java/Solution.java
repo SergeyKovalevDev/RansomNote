a3bd407348abebaf47efc80645317ae48cfa0ccd
Sergey Kovalev <dip.deep@mail.ru>
2022-06-05 17:43:38 +0300

    little changes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        boolean result = true;
         StringBuilder magazineSB = new StringBuilder(magazine);
         for (int i = 0; i < ransomNote.length(); i++) {
             char ch = ransomNote.charAt(i);
             int index = magazineSB.indexOf(String.valueOf(ch));
             if (index >= 0) magazineSB.deleteCharAt(index);
-            else {
-                result = false;
-                break;
-            }
+            else return false;
         }
-        return result;
+        return true;
     }
 }
